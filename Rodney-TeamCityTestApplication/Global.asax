afc4eb3ab7080d3e1683b078fcdd474593c43e9c
rc6886 <rc6886@gmail.com>
2015-12-05 18:54:17 -0600

    Adding initial MVC project.

---
+++ Rodney-TeamCityTestApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Rodney_TeamCityTestApplication.MvcApplication" Language="C#" %>
---
+++ Rodney-TeamCityTestApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Rodney_TeamCityTestApplication.Startup))]
+namespace Rodney_TeamCityTestApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
