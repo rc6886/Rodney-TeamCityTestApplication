41073518fa33daa0282ab15fd2f06216d3e5e4e4
Rodney Coleman <rodney.coleman@headspring.com>
2015-12-10 13:12:49 -0600

    Modified build file to add command for NuGet.

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
 @echo Executing psake build with default.ps1 configuration
 @echo off
+.\tools\NuGet\nuget.exe restore "Rodney-TeamCityTestApplication.sln" -source "https://nuget.org/api/v2/" -RequireConsent -o "packages"
+
 powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "& {Import-Module '.\psake.psm1'; invoke-psake .\default.ps1 %1 -parameters @{"version"="'%2'"}; if ($lastexitcode -ne 0) {write-host "ERROR: $lastexitcode" -fore RED; exit $lastexitcode} }" 
